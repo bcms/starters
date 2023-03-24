339602aa2aa52ce331440121c64fbe62c679e023
boris-becomes <boris.bosnjak@becomes.co>
2023-03-24 12:36:16 +0100

    Add Header

---
+++ nuxt/hospitale/website/api/_api-route.ts
@@ -0,0 +1,47 @@
+import type { BCMSMost, BCMSMostServerRoute } from "@becomes/cms-most/types";
+import { HeaderEntry, HeaderEntryMeta } from "~/bcms/types";
+import { APIResponse, Languages } from "~~/types";
+
+interface Route<Result = unknown, Body = unknown>
+  extends Omit<BCMSMostServerRoute<Result, Body>, "handler"> {
+  handler(data: {
+    url: string;
+    params: {
+      [name: string]: string;
+    };
+    query: {
+      [name: string]: string;
+    };
+    headers: {
+      [name: string]: string | string[] | undefined;
+    };
+    body: Body;
+    bcms: BCMSMost;
+    lng: Languages;
+  }): Promise<Result>;
+}
+
+export function apiRoute<Result = unknown, Body = unknown>(
+  route: Route<Result, Body>
+): Route<
+  APIResponse & {
+    data: Result;
+  },
+  Body
+> {
+  return {
+    method: route.method,
+    async handler(data) {
+      const lng = data.params.lng ? (data.params.lng as Languages) : "en";
+      const header = (await data.bcms.content.entry.findOne(
+        "header",
+        async (e) => e.meta.en.slug === "header"
+      )) as unknown as HeaderEntry;
+      const result = await route.handler(data);
+      return {
+        data: result,
+        header: header.meta[lng] as HeaderEntryMeta,
+      };
+    },
+  };
+}
--- nuxt/hospitale/website/pages/index.vue
+++ nuxt/hospitale/website/pages/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <PageWrapper v-if="data" :header="data.header">
+    <!--  -->
+  </PageWrapper>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { APIResponse, HomePageData } from "~~/types";
+
+const { data } = useAsyncData(async (ctx) => {
+  return await ctx?.$bcms.request<APIResponse<HomePageData>>({
+    url: "/home.json",
+  });
+});
+</script>
