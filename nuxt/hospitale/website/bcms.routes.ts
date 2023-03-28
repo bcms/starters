8f435ec44d4a85502e867b0b5e8c05cc681bf8c1
boris-becomes <boris.bosnjak@becomes.co>
2023-03-28 16:25:59 +0200

    Add about us page

--- nuxt/hospitale/website/bcms.routes.ts
+++ nuxt/hospitale/website/bcms.routes.ts
@@ -1,8 +1,10 @@
 import { createBcmsMostServerRoutes } from "@becomes/cms-most";
 import { ContactApi, HomeApi, JobsApi } from "./api";
+import { AboutApi } from "./api/about";
 
 export default createBcmsMostServerRoutes({
   ...HomeApi,
   ...JobsApi,
   ...ContactApi,
+  ...AboutApi,
 });
--- nuxt/hospitale/website/types/pages/index.ts
+++ nuxt/hospitale/website/types/pages/index.ts
@@ -1,3 +1,4 @@
 export * from "./home";
 export * from "./job";
 export * from "./contact";
+export * from "./about";
