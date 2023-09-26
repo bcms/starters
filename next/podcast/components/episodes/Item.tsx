1ff193c395d717dc3b9c5d18e3d3a1480763c38b
Suraj Auwal <siradjiawoual@gmail.com>
2023-09-26 06:23:25 +0100

    feature: next migration podcast starter

---
+++ next/podcast/components/episodes/Item.tsx
@@ -0,0 +1,165 @@
+import React, { useEffect, useRef, MouseEvent, useState } from 'react';
+import Link from 'next/link';
+import { BCMSImage } from 'next-plugin-bcms/components';
+import { bcmsMediaToUrl } from '@becomes/cms-most/frontend';
+import { EpisodeEntryMeta } from '@/bcms/types';
+import classnames from 'classnames';
+import { dateUtil } from '@/utils/date';
+import { usePlayer } from '@/context/PlayerContext';
+import PauseIcon from '@/assets/icons/pause.svg';
+import PlayIcon from '@/assets/icons/play.svg';
+
+interface EpisodeItemProps {
+  item: EpisodeEntryMeta;
+  index: number;
+  className?: string;
+}
+
+const EpisodeItem: React.FC<EpisodeItemProps> = ({
+  item,
+  index,
+  className,
+}) => {
+  const {
+    episode,
+    episodeDOM,
+    setEpisode,
+    setEpisodeDOM,
+    settings,
+    updateSettings,
+    getFileLength,
+  } = usePlayer();
+
+  const audioDOM = useRef<HTMLAudioElement | null>(null);
+  const [fileLength, setFileLength] = useState<string>('...');
+
+  const handlePlayPause = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if (!episode) {
+      setEpisode(item);
+      if (audioDOM) {
+        setEpisodeDOM(audioDOM.current as HTMLAudioElement);
+      }
+    } else {
+      if (episode.slug === item.slug) {
+        updateSettings({
+          playing: !settings.playing,
+        });
+      } else {
+        if (episodeDOM) {
+          updateSettings({
+            playing: false,
+          });
+          setEpisodeDOM(audioDOM.current as HTMLAudioElement);
+        }
+        setEpisode(item);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (!episode || episode.slug !== item.slug) {
+      updateSettings({ playing: true });
+    }
+  }, [episodeDOM]);
+
+  useEffect(() => {
+    const audio = new Audio(bcmsMediaToUrl(item.file));
+    audio.preload = 'metadata';
+    audio.addEventListener('loadedmetadata', () => {
+      audioDOM.current = audio;
+      const { durationInMinutes, durationInSeconds } = getFileLength(audio);
+
+      setFileLength(
+        `${durationInMinutes.toString().padStart(2, '0')}:${durationInSeconds
+          .toFixed(0)
+          .padStart(2, '0')}`,
+      );
+    });
+  }, [item, episode]);
+
+  const renderPlayButton: boolean = episode
+    ? (episode.slug === item.slug && !settings.playing) ||
+      episode.slug !== item.slug
+    : true;
+
+  const renderPauseButton: boolean = episode
+    ? episode.slug === item.slug && settings.playing
+    : false;
+
+  const renderIndex: boolean = !episode || episode.slug !== item.slug;
+  return (
+    <Link href={`/episode/${item.slug}`}>
+      <a
+        className={classnames(
+          'group grid grid-cols-[16px,24px,1fr,70px] gap-[5px] items-center px-[14px] py-2.5 text-left lg:grid-cols-[32px,64px,1fr,125px] lg:gap-4 lg:px-5 lg:py-6',
+          className,
+        )}
+      >
+        <button
+          className="flex items-center justify-center"
+          onClick={(e) => handlePlayPause(e)}
+        >
+          {renderIndex && (
+            <span className="text-xs leading-none font-medium tracking-[-0.8px] group-hover:hidden lg:text-xl lg:leading-none">
+              {index}
+            </span>
+          )}
+
+          {renderPlayButton && (
+            <PlayIcon
+              className={classnames(
+                'w-4 text-appAccent group-hover:inline-block lg:w-8',
+                {
+                  hidden: !episode || episode.slug !== item.slug,
+                },
+              )}
+            />
+          )}
+          {renderPauseButton && (
+            <PauseIcon className="w-4 text-appAccent lg:w-8" />
+          )}
+        </button>
+        <BCMSImage
+          media={item.cover}
+          options={{
+            sizes: {
+              exec: [
+                {
+                  width: 100,
+                  height: 100,
+                },
+              ],
+            },
+          }}
+          className="w-6 h-6 rounded cover overflow-hidden lg:w-16 lg:h-16"
+        />
+        <div className="max-lg:pl-2.5">
+          <div
+            className={
+              'text-sm leading-none font-medium tracking-[-0.8px] mb-1.5 line-clamp-1 lg:text-2xl lg:leading-none lg:mb-2.5'
+            }
+          >
+            {item.title}
+          </div>
+          <div className="text-xs leading-none tracking-[-0.8px] text-appGray-400 lg:text-xl lg:leading-none">
+            {item.guest?.meta?.en?.title || 'N / A'}
+          </div>
+        </div>
+        <div className="text-right">
+          <div>
+            <div className="text-sm leading-none font-medium tracking-[-0.8px] mb-1.5 lg:text-xl lg:leading-none lg:mb-2.5">
+              {fileLength}
+            </div>
+            <div className="text-xs leading-none font-medium tracking-[-0.8px] text-appGray-400 lg:text-xl lg:leading-none">
+              {dateUtil.format(item.date)}
+            </div>
+          </div>
+        </div>
+      </a>
+    </Link>
+  );
+};
+
+export default EpisodeItem;
---
+++ next/podcast/components/layout/PlayingEpisode.tsx
@@ -0,0 +1,176 @@
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import Link from 'next/link';
+import VolumeIcon from '@/assets/icons/volume.svg';
+import ForwardIcon from '@/assets/icons/forward.svg';
+import BackwardIcon from '@/assets/icons/backward.svg';
+import PauseIcon from '@/assets/icons/pause.svg';
+import PlayIcon from '@/assets/icons/play.svg';
+import { BCMSImage } from 'next-plugin-bcms/components';
+import { BCMSPropMediaDataParsed } from '@becomes/cms-client/types';
+import { usePlayer } from '@/context/PlayerContext';
+
+const NowPlaying = () => {
+  const {
+    episode,
+    settings,
+    updateSettings,
+    getPlayingEpisodeFileLength,
+    getCurrentPlayTime,
+    getEpisodeProgressBarWidth,
+    handlePrevEpisode,
+    handleNextEpisode,
+    handleRewind,
+  } = usePlayer();
+
+  const [fileLength, setFileLength] = useState('...');
+  const volumeWidth = `${settings.volume * 100}%`;
+
+  const handleVolumeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const target = event.target;
+    updateSettings({
+      volume: +target.value,
+    });
+  };
+
+  useEffect(() => {
+    const { durationInMinutes, durationInSeconds } =
+      getPlayingEpisodeFileLength();
+    setFileLength(
+      `${String(durationInMinutes).padStart(2, '0')}:${(durationInSeconds % 60)
+        .toFixed(0)
+        .padStart(2, '0')}`,
+    );
+  }, [episode]);
+
+  if (!episode) {
+    return <></>;
+  }
+  return (
+    <div className="fixed z-50 bottom-0 left-0 w-full bg-[#1F1F1F] py-2 lg:bg-appBody lg:border-t lg:border-appGray-600 lg:py-6">
+      <div className="container">
+        <div className="flex items-center justify-between xl:gap-10">
+          <Link href="/now-playing" passHref>
+            <a className="flex items-center lg:flex-1">
+              {episode && (
+                <BCMSImage
+                  media={episode?.cover as unknown as BCMSPropMediaDataParsed}
+                  options={{
+                    sizes: {
+                      exec: [
+                        {
+                          width: 100,
+                          height: 100,
+                        },
+                      ],
+                    },
+                  }}
+                  className="w-8 h-8 rounded cover overflow-hidden mr-2.5 lg:w-14 lg:h-14 lg:mr-4"
+                />
+              )}
+              <div>
+                <div className="text-sm leading-none font-medium tracking-[-0.8px] mb-1.5 line-clamp-1 lg:text-lg lg:leading-none lg:mb-2.5">
+                  {episode?.title ?? ''}
+                </div>
+                <div className="text-xs leading-none tracking-[-0.8px] text-appGray-400 lg:text-base lg:leading-none">
+                  {episode?.guest?.meta?.en?.title ?? 'N / A'}
+                </div>
+              </div>
+            </a>
+          </Link>
+          <div className="flex flex-col items-center justify-center flex-1 max-lg:hidden">
+            <div className="flex items-center space-x-7 mb-2">
+              <button
+                className="text-xl leading-none tracking-[-0.8px] text-appGray-400 min-w-max max-xl:hidden"
+                onClick={handlePrevEpisode}
+              >
+                Previous episode
+              </button>
+              <button className="flex" onClick={handlePrevEpisode}>
+                <BackwardIcon className="w-6 h-6" />
+              </button>
+              <button
+                className="flex items-center justify-center flex-shrink-0 w-8 h-8 bg-white rounded-full"
+                onClick={() =>
+                  updateSettings({
+                    playing: !settings.playing,
+                  })
+                }
+              >
+                {settings.playing ? (
+                  <PauseIcon className="text-appAccent w-6 h-6" />
+                ) : (
+                  <PlayIcon className="text-appAccent w-6 h-6" />
+                )}
+              </button>
+              <button className="flex" onClick={handleNextEpisode}>
+                <ForwardIcon className="w-6 h-6" />
+              </button>
+              <button
+                className="text-xl leading-none tracking-[-0.8px] text-appGray-400 min-w-max max-xl:hidden"
+                onClick={handleNextEpisode}
+              >
+                Next episode
+              </button>
+            </div>
+            <div className="flex items-center space-x-2.5 xl:w-full">
+              <div className="leading-none tracking-[-0.8px] text-appGray-400">
+                {getCurrentPlayTime()}
+              </div>
+              <div className="relative flex-1">
+                <div className="relative w-[128px] h-1 rounded overflow-hidden bg-appGray-800 xl:w-full">
+                  <div
+                    className="absolute top-0 left-0 h-full bg-white rounded"
+                    style={{ width: getEpisodeProgressBarWidth() }}
+                  />
+                </div>
+                <div
+                  className="absolute top-1/2 -translate-y-1/2 left-0 w-full h-5 rounded cursor-pointer"
+                  onClick={handleRewind}
+                />
+              </div>
+              <div className="leading-none tracking-[-0.8px] text-appGray-400">
+                {fileLength}
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center justify-end flex-1 max-lg:hidden">
+            <VolumeIcon className="w-6 h-6 mr-4" />
+            <label className="relative w-[128px]">
+              <div className="relative h-1 rounded overflow-hidden bg-appGray-800">
+                <div
+                  className="absolute top-0 left-0 h-full bg-white rounded"
+                  style={{ width: volumeWidth }}
+                />
+              </div>
+              <input
+                value={settings.volume}
+                type="range"
+                step="0.01"
+                min="0"
+                max="1"
+                className="absolute top-1/2 -translate-y-1/2 left-0 w-full h-5 opacity-0 cursor-pointer"
+                onChange={(e) => handleVolumeChange(e)}
+              />
+            </label>
+          </div>
+          <button
+            className="flex lg:hidden"
+            onClick={() =>
+              updateSettings({
+                playing: !settings.playing,
+              })
+            }
+          >
+            {settings.playing ? (
+              <PauseIcon className="w-5 h-5" />
+            ) : (
+              <PlayIcon className="w-5 h-5" />
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NowPlaying;
