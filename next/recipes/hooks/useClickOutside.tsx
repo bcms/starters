7b6dffaad6955bb109204adaeefe6277f1f4d968
siradji <siradjiawoual@gmail.com>
2023-10-15 12:52:59 +0100

    feature: recipers starter

---
+++ next/recipes/hooks/useClickOutside.tsx
@@ -0,0 +1,27 @@
+import { useEffect, RefObject } from 'react';
+
+interface ClickOutsideOptions {
+  ref: RefObject<HTMLElement>;
+  callback: () => void;
+}
+
+export const useClickOutside = ({
+  callback,
+  ref,
+}: ClickOutsideOptions): void => {
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback();
+      }
+
+      return;
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref, callback]);
+};
