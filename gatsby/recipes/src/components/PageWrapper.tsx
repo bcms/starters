19bcb39bf1055d03eedca4c7b0b0d2e7c42844c1
Suraj Auwal <surajtauwal@gmail.com>
2023-11-20 10:12:59 +0100

    fix: added Inter font

--- gatsby/recipes/src/components/PageWrapper.tsx
+++ gatsby/recipes/src/components/PageWrapper.tsx
@@ -65,6 +65,10 @@ export const PageWrapper: FC<PropsWithChildren<PageData>> = ({
           rel="stylesheet"
         />
         <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
           rel="icon"
           type="image/png"
           sizes="32x32"
