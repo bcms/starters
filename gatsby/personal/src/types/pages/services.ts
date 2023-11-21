5102a33a6b1ce2a867a8a599a87621d4bc4f5070
Mo Awwal <gomandev@gmail.com>
2023-11-21 11:29:01 +0000

    fix: removed personal folder

--- gatsby/personal/src/types/pages/services.ts
+++
@@ -1,9 +0,0 @@
-import {
-  ServiceItemEntryMeta,
-  ServicesPageEntryMeta,
-} from '../../../bcms/types';
-
-export interface ServicesPageData {
-  meta: ServicesPageEntryMeta;
-  services: ServiceItemEntryMeta[];
-}
