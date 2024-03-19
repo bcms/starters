b5fc14428037a5a14cc93d92dc56d7cda31a4310
boris-becomes <boris.bosnjak@becomes.co>
2024-03-19 12:31:24 +0100

    feat: portfolio page

--- nuxt/agency/pages/portfolio.vue
+++ nuxt/agency/pages/portfolio.vue
@@ -2,38 +2,38 @@
   <PageWrapper v-if="data" :header="data.header" :footer="data.footer">
     <Hero
       :title="data.page.meta.title"
-      subtitle="Our services"
+      subtitle="Portfolio"
       :description="data.page.meta.description"
     />
-    <ServicesList :services="data.page.meta.services" />
+    <PortfolioList :items="data.page.meta.items" />
     <ContactBlock :data="data.page.meta.contact_block" />
   </PageWrapper>
 </template>
 
 <script setup lang="ts">
 import { NuxtApp } from 'nuxt/app';
-import { ServicesPageEntry, ServicesPageEntryMeta } from '~~/bcms/types';
-import { PageProps, ServicesPageData } from '~~/types';
+import { PortfolioPageEntry, PortfolioPageEntryMeta } from '~~/bcms/types';
+import { PageProps, PortfolioPageData } from '~~/types';
 import { getHeaderAndFooter } from '~~/utils/page-data';
 
-const { data, error } = useAsyncData<PageProps<ServicesPageData> | undefined>(
-  'services',
+const { data, error } = useAsyncData<PageProps<PortfolioPageData> | undefined>(
+  'portfolio',
   async (ctx) => {
     const { header, footer } = await getHeaderAndFooter(ctx as NuxtApp);
-    // Get Services Page entry
-    const servicesPage = (await ctx?.$bcms.entry.get({
-      template: 'services_page',
-      entry: 'services',
-    })) as ServicesPageEntry;
-    if (!servicesPage) {
-      throw new Error('Services page entry does not exist.');
+    // Get Portfolio Page entry
+    const portfolioPage = (await ctx?.$bcms.entry.get({
+      template: 'portfolio_page',
+      entry: 'portfolio',
+    })) as PortfolioPageEntry;
+    if (!portfolioPage) {
+      throw new Error('Portfolio page entry does not exist.');
     }
     return {
       header,
       footer,
       page: {
-        // Return Services Page entry for `en` locale
-        meta: servicesPage.meta.en as ServicesPageEntryMeta,
+        // Return Portfolio Page entry for `en` locale
+        meta: portfolioPage.meta.en as PortfolioPageEntryMeta,
       },
     };
   },
