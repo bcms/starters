d1782657d24aaa001251453f1592133c2ee35b66
bbanez <banezprod@gmail.com>
2025-04-23 12:49:26 +0200

    update conference to next 15 and react 19

--- next/conference/src/app/page.tsx
+++ next/conference/src/app/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { bcms } from './bcms-client';
 import { Metadata } from 'next';
 import {
     HomePageEntry,
@@ -14,6 +13,8 @@ import HomeTickets from '@/components/home-page/Tickets';
 import HomeAgenda from '@/components/home-page/Agenda';
 import PageWrapper from '@/components/PageWrapper';
 import 'swiper/css';
+import { bcmsPrivate } from '@/app/bcms-private';
+import { bcmsPublic } from '@/app/bcms-public';
 
 const pageTitle = 'Home - CONference';
 export const metadata: Metadata = {
@@ -27,14 +28,16 @@ export const metadata: Metadata = {
 };
 
 const HomePage: React.FC = async () => {
-    const homePageEntry = (await bcms.entry.getBySlug(
+    const homePageEntry = (await bcmsPrivate.entry.getBySlug(
         'home',
         'home-page',
     )) as HomePageEntry;
 
     const homePageMeta = homePageEntry.meta.en as HomePageEntryMetaItem;
 
-    const legalEntries = (await bcms.entry.getAll('legal')) as LegalEntry[];
+    const legalEntries = (await bcmsPrivate.entry.getAll(
+        'legal',
+    )) as LegalEntry[];
 
     return (
         <PageWrapper legal={legalEntries}>
@@ -42,32 +45,32 @@ const HomePage: React.FC = async () => {
                 gallery={homePageMeta.hero_gallery}
                 description={homePageMeta.hero_description}
                 cover={homePageMeta.hero_cover_image}
-                bcmsConfig={bcms.getConfig()}
+                bcmsConfig={bcmsPublic.getConfig()}
             />
             <HomeAbout
                 title={homePageMeta.about_title}
                 cover={homePageMeta.about_cover}
                 description={homePageMeta.about_description}
                 topics={homePageMeta.about_topics}
-                bcmsConfig={bcms.getConfig()}
+                bcmsConfig={bcmsPublic.getConfig()}
             />
             <HomeSpeakers
                 title={homePageMeta.speakers_title}
                 description={homePageMeta.speakers_description}
                 cover={homePageMeta.speakers_cover_image}
                 speakers={homePageMeta.speakers}
-                bcmsConfig={bcms.getConfig()}
+                bcmsConfig={bcmsPublic.getConfig()}
             />
             <HomeSponsors
                 title={homePageMeta.sponsors_title}
                 sponsors={homePageMeta.sponsors}
-                bcmsConfig={bcms.getConfig()}
+                bcmsConfig={bcmsPublic.getConfig()}
             />
             <HomeTickets
                 title={homePageMeta.tickets_title}
                 description={homePageMeta.tickets_description}
                 tickets={homePageMeta.tickets}
-                bcmsConfig={bcms.getConfig()}
+                bcmsConfig={bcmsPublic.getConfig()}
             />
             <HomeAgenda
                 title={homePageMeta.agenda_title}
