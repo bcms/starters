dd5db2bb8d79c98ed2d4d47b8e2cf93fb7946735
momciloo <popovmomcilo@gmail.com>
2026-03-13 12:01:40 +0100

    chore(bcms-client): add comment for API key usage in multiple client files

--- next/blog/src/app/bcms-private.ts
+++ next/blog/src/app/bcms-private.ts
@@ -1,3 +1,4 @@
 import { Client } from '@thebcms/client';
 
+// If not provided, BCMS uses BCMS_API_KEY environment variable for the apiKey parameter
 export const bcmsPrivate = new Client({ injectSvg: true });
