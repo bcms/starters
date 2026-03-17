f108738271d44a1617052989732039a8d4ecf1bc
momciloo <popovmomcilo@gmail.com>
2026-03-17 19:09:58 +0100

    remove font configuration for 'Inter' from astro.config; retain integrations for Tailwind and React

--- astro/e-commerce/astro.config.mjs
+++ astro/e-commerce/astro.config.mjs
@@ -1,18 +1,11 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), react()],
-    fonts: [
-        {
-            name: 'Inter',
-            cssVariable: '--font-inter',
-            provider: fontProviders.google(),
-        },
-    ],
     security: {
         csp: true,
     },
